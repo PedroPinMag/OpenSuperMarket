c64509860c25d130760c0da27e7f586c978869e0
Pedro Pinheiro Magalhães <71952546+PedroPinMag@users.noreply.github.com>
2025-09-06 19:44:21 -0300

    Apps Criados e .env

--- OpenSuperMarket/settings.py
+++ OpenSuperMarket/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xzr#)(ls*#lebx6d-i+6@fm-#+v@73p#$t2h_n5&0f1lm-z6w6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
